c67c97f13a71ac788096f187c3d6d679e986ba1b
Emerson Santiago Venancio <emersan@msn.com>
2018-08-02 11:20:47 -0300

    adcionando bootstrap css js e glyphicons

--- index.php
+++ index.php
@@ -2,9 +2,38 @@
 	session_start();
 ?>		
 
-<h1>Emiolo - API - Star Wars</h1>		<hr />		
-<head>
-</head>	
+    <h1>Emiolo - API - Star Wars</h1>		<hr />		
+        
+        <head>
+            <script src="<?php if (file_exists("js/jquery.min.js")){ echo $url = "js/jquery.min.js"; }  
+                else { echo $url = "../js/jquery.min.js"; } ?>">
+            </script>
+
+            <script src="<?php if (file_exists("js/bootstrap.min.js")){ echo $url = "js/bootstrap.min.js"; }  
+                else { echo $url = "../js/bootstrap.min.js"; } ?>">
+            </script>       
+
+            <link rel="stylesheet" href="<?php 
+                if (file_exists("css/bootstrap.min.css")){ echo $url = "css/bootstrap.min.css"; }  
+                else { echo $url = "../css/bootstrap.min.css"; } 
+            ?>" />
+
+            <link rel="stylesheet" href="<?php 
+                if (file_exists("css/bootstrap-responsive.min.css")){ echo $url = "css/bootstrap-responsive.min.css"; }  
+                else { echo $url = "../css/bootstrap-responsive.min.css"; } 
+            ?>" />
+
+            <link rel="stylesheet" href="<?php 
+                if (file_exists("css/matrix-style.css")){ echo $url = "css/matrix-style.css"; }  
+                else { echo $url = "../css/matrix-style.css"; } 
+            ?>" />
+
+            <link href="<?php 
+                if (file_exists("css/font-awesome.css")){ echo $url = "css/font-awesome.css"; }  
+                else { echo $url = "../css/font-awesome.css"; } 
+            ?>" rel="stylesheet" />
+        </head>
+
 <body>	
   <ul class="quick-actions">
          <li class="bg_ly"> <a href=""> <i class="glyphicon glyphicon-inbox" style="color:white;font-size:2.6em"></i><span class="label label-success"></span></br></br>APIs </a> </li>
