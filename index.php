7c75488a63876e4538eb084b88af95449e7158d7
Emerson Santiago Venancio <emersan@msn.com>
2018-08-02 11:09:33 -0300

    adicionando Index.php - teste commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php  
+	session_start();
+?>		
+
+<h1>Emiolo - API - Star Wars</h1>		<hr />		
+<head>
+</head>	
+<body>	
+  <ul class="quick-actions">
+         <li class="bg_ly"> <a href=""> <i class="glyphicon glyphicon-inbox" style="color:white;font-size:2.6em"></i><span class="label label-success"></span></br></br>APIs </a> </li>
+      </ul>
+</body>	
